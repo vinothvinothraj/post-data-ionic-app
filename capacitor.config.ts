8c9f5609160b6123db8912d88811fc3edfe73080
Vinoth07 <sangaralingamvinoth64@gmail.com>
2024-11-14 09:40:00 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'learnRoutesNew',
+  webDir: 'www'
+};
+
+export default config;
